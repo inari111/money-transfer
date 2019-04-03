257050e8a75b0b6a05b3050df9875e88192b8e4e
inari111 <takuyakoori+github@gmail.com>
2019-04-03 14:57:20 +0900

    add money table

--- _docker/mysql/init.sql
+++ _docker/mysql/init.sql
@@ -10,3 +10,11 @@ CREATE TABLE `user` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `money` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
